726b384e870806cba27d4a9dc951b13d2e6081a1
Бык Дмитрий Сергеевич <okb58@mtz.by>
2022-06-13 15:26:39 +0300

    Initial commit

---
+++ TestsUI.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture(autouse=True)
+def testing():
+   pytest.driver = webdriver.Chrome('C:/Program Files/ChromeDriver/chromedriver.exe')
+   # Переходим на страницу авторизации
+   pytest.driver.get('https://petfriends.skillfactory.ru/login')
+
+   yield
+
+   pytest.driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('dima-pinsk@mail.ru')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('nanotehnik444')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+
+def test_my_pets():
+   images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+   names = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+   descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+
+   for i in range(len(names)):
+      assert images[i].get_attribute('src') != ''
+      assert names[i].text != ''
+      assert descriptions[i].text != ''
+      assert ', ' in descriptions[i]
+      parts = descriptions[i].text.split(", ")
+      assert len(parts[0]) > 0
+      assert len(parts[1]) > 0
