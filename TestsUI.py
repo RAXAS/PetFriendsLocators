2e6f3e7d67ca030174748ffa01dcad275c59f138
RAXAS <dima-pinsk@mail.ru>
2022-06-16 10:38:07 +0300

    Initial commit

--- TestsUI.py
+++ TestsUI.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from collections import Counter
 
 
 def chrome_options(chrome_options):
@@ -31,7 +32,7 @@ def test_my_pets():
    # Проверяем, что мы оказались на странице "мои питомцы"
    assert pytest.driver.find_element_by_css_selector("html>body>div>div>div:nth-of-type(2)")
 
-   # Для css - $$("")
+   # Для поиска css - $$("")
    # Получаем все фотографии питомцев на странице
    images = pytest.driver.find_elements_by_css_selector('div th > img')
    # Получаем всю информацию о питомцах на странице
@@ -40,20 +41,39 @@ def test_my_pets():
    names = pets_info[::5]
    breeds = pets_info[1::5]
    ages = pets_info[2::5]
+   # Получаем информацию профиля и записываем в переменную
+   amount = pytest.driver.find_elements_by_css_selector('html > body > div > div > div')
+   # Создаём переменную для подсчёта колличества питомцев с фото
+   pets_with_photo = 0
+   # Создаём пустой список, в которые будем записывать имена питомцев
+   list_names = []
 
-   #body > div.task2.fill > div > div.\.col-sm-4.left
-   #amount = pytest.driver.find_elements_by_css_selector('//*[contains(text(), “Питомцев:”)]')
-   #print('amount')
-   # Проверяем, что у питомцев присутствуют: фотография, имя, порода и возраст
    for i in range(len(names)):
-      # Проверяем, что у питомцев присутствуют: фотография
-      assert images[i].get_attribute('img') != ''
+      # Записываем колличество питомцев с фотографией
+      if images[i].get_attribute('img') != '':
+         pets_with_photo += 1
       # Проверяем, что у всех питомцев присутствует имя
       assert names[i].text != ''
       # Проверяем, что у всех питомцев присутствует порода
       assert breeds[i].text != ''
       # Проверяем, что у всех питомцев присутствует возраст
       assert ages[i].text != ''
+      # Добавляем имена питомцев в список
+      list_names.append(names[i].text)
+   # Проверяем, что имя каждого животного уникально
+   #for i in range(len(names)):
+      #a = Counter(list_names)
+      #print(a[i])
+      #assert Counter(list_names)[i] == 1
 
-      #assert amount[i].text != ''
+
+
+   # Проверяем, что количество строк таблицы соответствует количеству питомцев в блоке статистики пользователя
+   assert f"Питомцев: {len(names)}" in amount[0].text
+   # Проверяем, что минимум у половины питомцев присутствует фотография
+   assert pets_with_photo >= len(names) / 2
+   # Проверяем, что у каждого питомца разное имя
+   #assert
+
+
 
